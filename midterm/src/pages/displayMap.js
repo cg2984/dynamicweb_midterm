44393c0139feb8f4b1eac01bc6e5360ca66afc96
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2020-03-30 10:12:17 -0400

    got tristate area to deliver data and push it to an array

--- midterm/src/pages/displayMap.js
+++ midterm/src/pages/displayMap.js
@@ -10,6 +10,7 @@ import Header from "../components/header.js"
 let lat = 38.4506;
 let lon = -96.1133;
 let zoom = 3.4;
+
 let pins = "pin-s-a+9ed4bd(-154.493062,63.588753),pin-s-b+000(-86.902298,32.318231),pin-s-b+000(-91.831833,35.20105),pin-s-b+000(-111.093731,34.048928),pin-s-b+000(-119.417932,36.778261),pin-s-b+000(-105.782067,39.550051),pin-s-b+000(-73.087749,41.603221),pin-s-b+000(-77.033418,38.905985)";
 
 //---COLORS-----------------------
@@ -35,11 +36,13 @@ let mapUrl = baseUrl+endpoint+token
 
 //---GHG VARIABLES
 const key = "UcUGqUyJDvEldhwGumvpyxxmNaIRgGRHjJqa8Tde";
-//let endpoint_GHG = `api/solar/solar_resource/v1.json?api_key=${key}&lat=${lat}&lon=${lon}`;
+let dataColors = [];
+let dataArray = [];
+//const stateArray = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY","DC"];
+const stateArray = ["NY","CT","NJ"];
+let i = 0;
 
-const stateArray = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY","DC"];
-const stateArrayLen = stateArray.length-1
-//let i = 0;
+
 	
 function DisplayMap(){
 	let history = useHistory();
@@ -47,96 +50,92 @@ function DisplayMap(){
 	const[mapData,setMapData] = useState({});
 	const[map,setMap] = useState("");
 	const[year,setYear] = useState("");
-	const[type, setType] = useState("total");
+	const[type, setType] = useState(null);
 	const[GHGData, setGHGData] = useState({});
 	const[mapTest,setMapTest] = useState("");
 	const[emissions,setEmissions] = useState(0);
+	const[emissionsNY,setEmissionsNY] = useState(0);
+	const[emissionsNJ,setEmissionsNJ] = useState(0);
+	const[emissionsCT,setEmissionsCT] = useState(0);
 
 	useEffect(() => {
 		let mySearchParams = history.location.search;
 		let urlParams = new URLSearchParams(mySearchParams);
 		let type = urlParams.get('type');
-		if(type){
-			setType(type);
-			console.log("type",type);
-		}
+		setType(type);
+		// let year = urlParams.get('year');
+		// if(type && year){
+		// 	setType(type);
+		// 	console.log("type",type);
+		// 	setYear(year);
+		// }
 	},[history]);
 
-	//getting the solar data 
+	console.log(type);
+
+	//getting the GHG data 
 	function getGHGData(){
-		axios.get(`https://developer.nrel.gov/api/cleap/v1/state_co2_emissions?state_abbr=NY&type=${type}&api_key=${key}`)
+		axios.get(`https://developer.nrel.gov/api/cleap/v1/state_co2_emissions?state_abbr=${stateArray[i]}&type=${type}&api_key=${key}`)
+			.then(function (response) {
+				// handle success
+					setGHGData(response);
+					console.log(GHGData);
+					console.log(type);
+				})
+				.catch(function (error) {
+					// handle error
+					console.log(error);
+				})
+				.then(function () {
+					// always executed
+			});
+	}	
+
+	//type != null here because when i put it on the axiox function it didnt work. this keeps it from loading the default value for type
+	if (type!=null){
+		for(i;i < 3;i++){
+		getGHGData();
+	 	console.log(i,stateArray[i]);
+		}
+	}
+	
+	
+	
+//loading ghg data into variables. without this only a proto object thing will load
+	useEffect(() => {
+		//must have the .data or else it will give you an error because it wont load properly and it will give undefined error
+		if(GHGData.data){
+			setYear(GHGData.data.result[0].start);
+			setEmissions(GHGData.data.result[0].data[2000]);
+			console.log(emissions,GHGData);
+			if(emissions>)
+			dataArray.push(GHGData.data.result[0].data[2000]);
+			console.log(dataArray);
+		}
+	},[GHGData]);
+	
+//-----------------------------------------MAPS
+
+	//getting the map data
+	useEffect(() => {
+		if(dataArray.len==stateArray.len && dataArray.len > 0){
+			//normally its mapurl
+			axios.get(mapUrl)
 				.then(function (response) {
 				// handle success
-				setGHGData(response);
+				console.log("map data success");
+				console.log(response);
+				setMapData(response);
 			})
 			.catch(function (error) {
 				// handle error
 				console.log(error);
 			})
 			.then(function () {
-				// always executed
-		});
-	}	
-
-	useEffect(() => {
-		getGHGData();
-	},[type]);
-
-	
-	// for(i=0;i < stateArrayLen;i++){
-	// 	console.log(stateArray[i]);
-	// 	getGHGData();
-	// 	setTimeout(getGHGData(),5000);
-	// }
-
-	let ghgDataArray = [];
-
-	//loading ghg data into variables, it automatically loads for each run of the for loop
-	useEffect(() => {
-		//must have the .data or else it will give you an error because it wont load properly
-		if(GHGData.data){
-			setYear(GHGData.data.result[0].start);
-			setEmissions(GHGData.data.result[0].data[2000]);
-			console.log(emissions,GHGData);
+				//always executed
+			});
 		}
-	},[GHGData]);
-	
-
-	//getting the map data
-	useEffect(() => {
-		//normally its mapurl
-		axios.get(mapUrl)
-			.then(function (response) {
-			// handle success
-			console.log("map data success");
-			console.log(response);
-			setMapData(response);
-		})
-		.catch(function (error) {
-			// handle error
-			console.log(error);
-		})
-		.then(function () {
-			//always executed
-		});
 	},[]);
-
-	// useEffect(() => {
-	// 	axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/NY.json?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw&cachebuster=1585440334290&autocomplete=true&types=region`)
-	// 		.then(function (response) {
-	// 		// handle success
-	// 		console.log("map geocoding");
-	// 		console.log(response);
-	// 		setMapData(response);
-	// 	})
-	// 	.catch(function (error) {
-	// 		// handle error
-	// 		console.log(error);
-	// 	})
-	// 	.then(function () {
-	// 		//always executed
-	// 	});
-	// },[]);
 
 	//loading map data into variables
 	useEffect(() => {
