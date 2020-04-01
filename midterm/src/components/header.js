e47e7ee5ad86d60908979365ac4e21debe9e53b1
Clara <cg2984@nyu.edu>
2020-03-31 22:30:09 -0400

    cleaning up

--- midterm/src/components/header.js
+++ midterm/src/components/header.js
@@ -1,63 +1,33 @@
 import React, {useEffect,useState} from 'react';
+function Header({usState}){
 
-let display = "flex";
-
-function Header({type}) {
-	// function CloseMenu(display){
-	// 	console.log("click");
-	// 	if(display=="none"){
-	// 		let display = "flex";
-	// 	}
-	// 	else{
-	// 		let display = "none";
-	// 	}
-	// };
 
 	return(
 		<div className = "header">
 			<div className = "header_text">
-				<h1 className = "headerLink">{type}</h1>
+				<h1 className = "headerLink">{usState}</h1>
 				<h1>GHG EMISSIONS IN</h1>
 				<h1 className = "headerLink">2000</h1>
 			</div>
-			<div style={{display:`${display}`}}className = "buttonWrapper_type">
+			<div className = "buttonWrapper_type">
 				<div className = "button">
-					<a href = "/?type=industrial">INDUSTRIAL</a>
+					<a href = "/?type=NY">NEW YORK</a>
 				</div>
 				<div className = "button">
-					<a href = "/?type=commercial">COMMERCIAL</a>
+					<a href = "/?type=CT">CONNETICUT</a>
 				</div>
 				<div className = "button">
-					<a href = "/?type=electric">ELECTRIC</a>
+					<a href = "/?type=NJ">New jersey</a>
 				</div>
 				<div className = "button">
-					<a href = "/?type=residential">RESIDENTIAL</a>
+					<a href = "/?type=PA">Pennsylvania</a>
 				</div>
 				<div className = "button">
-					<a href = "/?type=transportation">TRANSPORTATION</a>
-				</div>
-				<div className = "button">
-					<a href = "/?type=total">TOTAL</a>
-				</div>
-				<div className = 'dates'>
-				</div>
-			</div>
-			<div style={{display:`${display}`}}className = "buttonWrapper_year">
-				<div className = "button">
-					<a href = "&year=1980">1980</a>
-				</div>
-				<div className = "button">
-					<a href = "&year=1990">1990</a>
-				</div>
-				<div className = "button">
-					<a href = "&year=2000">2000</a>
-				</div>
-				<div className = "button">
-					<a href = "&year=2014">2014</a>
+					<a href = "/?type=MA">Massachussets</a>
 				</div>
 			</div>
 		</div>
-	)
+	);
 }
 
 export default Header
--- midterm/src/pages/displayMap.js
+++ midterm/src/pages/displayMap.js
@@ -5,185 +5,98 @@ import { useHistory } from "react-router-dom";
 import Header from "../components/header.js"
 
 //---OTHER VARIABLES
-//southmost longistude: 18.55
-//northmost 71.23
 let lat = 40.730610;
 let lon = -73.935242;
 let zoom = 5;
 
-//---COLORS-----------------------
-//250-201 => #FF431B
-//200-151 => #FF7C1B
-//150-101 => #FFB51B
-//100-51 => #FFEE1B
-//50-0 => #D7FF1B
-
 
 //---GHG VARIABLES
 const key = "UcUGqUyJDvEldhwGumvpyxxmNaIRgGRHjJqa8Tde";
-let dataColor = 0;
-let dataArray = [];
 
-//const stateArray = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY","DC"];
-const stateArray = ["NY","CT","NJ","PA"];
-let i = 0;
-let j = 0;
-
-let pin0 = "";
-let pin1 = "";
-let pin2 = "";
-let pin3 = "";
-
-
-let dataColorArray = [];
-let pins = null;
-let endpoint = `styles/v1/mapbox/light-v10/static/${pins}/${lon},${lat},${zoom}/1080x512`;
+//MAP
+let endpoint = `styles/v1/mapbox/light-v10/static/${lon},${lat},${zoom}/1080x512`;
 const token = "?access_token=pk.eyJ1IjoiY2cyOTg0IiwiYSI6ImNrODRpbnNlbjAwOWczZm8ybXM5azBuZnYifQ.0cD8Ldn1qLXkLW5331lmCg";
 const baseUrl = "https://api.mapbox.com/";
-let mapUrlInitial = baseUrl+endpoint+token;
+let mapUrl = baseUrl+endpoint+token;
 	
+let mapData;
+let GHG;
 function DisplayMap(){
 	let history = useHistory();
 
 	const[mapData,setMapData] = useState({});
 	const[map,setMap] = useState("");
-	const[year,setYear] = useState("");
-	const[type, setType] = useState(null);
-	const[GHGData, setGHGData] = useState({});
-	const[mapTest,setMapTest] = useState("");
-	const[emissions,setEmissions] = useState(0);
+	const[emissions,setEmissions] = useState(0)
+	const[color,setColor] = useState("FFFFFF")
+	const[usState,setUsState] = useState("NY");
 
+
+	// useEffect(() => {
+	// 	let mySearchParams = history.location.search;
+	// 	let urlParams = new URLSearchParams(mySearchParams);
+	// 	let usState = urlParams.get('usState');
+	// 	setType(usState);
+	// },[history]);
 
 	useEffect(() => {
-		let mySearchParams = history.location.search;
-		let urlParams = new URLSearchParams(mySearchParams);
-		let type = urlParams.get('type');
-		setType(type);
-		// let year = urlParams.get('year');
-		// if(type && year){
-		// 	setType(type);
-		// 	console.log("type",type);
-		// 	setYear(year);
-		// }
-	},[history]);
-
-	//getting the GHG data 
-	function getGHGData(){
-		axios.get(`https://developer.nrel.gov/api/cleap/v1/state_co2_emissions?state_abbr=${stateArray[i]}&type=${type}&api_key=${key}`)
+		axios.get(`https://developer.nrel.gov/api/cleap/v1/state_co2_emissions?state_abbr=NY&type=total&api_key=${key}`)
 			.then(function (response) {
-				// handle success
-					setGHGData(response);
-					console.log(GHGData);
-					console.log("request type",type);
-				})
-				.catch(function (error) {
-					// handle error
-					console.log(error);
-				})
-				.then(function () {
-					// always executed
-			});
-	}	
-
-	//type != null here because when i put it on the axios function it didnt work. this keeps it from loading the default value for type
-	if (type!=null){
-		for(i;i < 4;i++){
-		getGHGData();
-	 	console.log(i,stateArray[i]);
-		}
-	}
-	
-	
-	
-//loading ghg data into variables. without this only a proto object thing will load
-	useEffect(() => {
-		//must have the .data or else it will give you an error because it wont load properly and it will give undefined error
-		if(GHGData.data){
-			setYear(GHGData.data.result[0].start);
-			setEmissions(GHGData.data.result[0].data[2000]);
-			console.log(emissions,GHGData);
-			dataArray.push(emissions);
-			//console.log("dataColor",dataColor);
-			//dataColorArray.push(dataColor);
-			//console.log("dataArray", dataArray);
-		}
-	},[GHGData]);
-
-//--MAPS--------------------------------------------------------------------------------------------------------
-	function colorSet(){
-		for(j;j < 5;j++){
-			if(250 > dataArray[j] > 201){
-				dataColor="FF431B"
-			} 
-			if(200 > dataArray[j] > 151){
-				dataColor="FF7C1B"
-			}
-			if(150 > dataArray[j] > 101){
-				dataColor="FFB51B"
-			} 					
-			if(100 > dataArray[j] > 51){
-				dataColor="FFEE1B"
-			}
-			if(50 > dataArray[j] > 0){
-				dataColor="D7FF1B"
-			}
-			else{
-				dataColor = "FFFFFF";
-			}
-
-			dataColorArray.push(dataColor);
-			console.log(dataColorArray)
-		}
-		let pins = `pin-s-a+${dataColorArray[0]}(-75.420735,43.145888),pin-s-b+${dataColorArray[1]}(-74.4057,40.0583),pin-s-b+${dataColorArray[2]}(-73.0877,41.6032),pin-s-b+${dataColorArray[3]}(-77.466611,40.873649)`;
-	}	
-
-	
-	function getMapData(){
-		//if(pins != null){
-			//normally its mapurl
-			axios.get(mapUrlInitial)
-				.then(function (response) {
-				// handle success
-				console.log("map data success");
-				console.log(response);
-				setMapData(response);
-			})
-			.catch(function (error) {
-				// handle error
-				console.log(error);
-			})
-			.then(function () {
-				//always executed
-			});
-		//}	
-	}
-
-
-	//getting the map data
-	useEffect(() => {
-	   if(pins != null){
-	   		colorSet();
-			getMapData();
-		}
+			// handle success
+			console.log(response);
+			let GHG = response
+			console.log("success",GHG);
+		})
+		.catch(function (error) {
+			// handle error
+			console.log(error);
+		})
+		.then(function () {
+			// always executed
+		});
 	},[]);
 
-	//loading map data into variables
 	useEffect(() => {
-		if(mapData.config){
-			setMap(mapData.data);
-			console.log(mapData.config.url);
-		}
-	},[mapData]);
+		axios.get(mapUrl)
+			.then(function (response) {
+			// handle success
+			console.log(response);
+			setMapData(response);
+			console.log("mapData",mapData);
+		})
+		.catch(function (error) {
+			// handle error
+			console.log(error);
+		})
+		.then(function () {
+			// always executed
+		});
+	},[]);
+
+	
+	//loading ghg data into variables. without this only a proto object thing will load
+	// useEffect(() => {
+	// 	//must have the .data or else it will give you an error because it wont load properly and it will give undefined error
+	// 	if(GHGData.data){
+	// 		setEmissions(GHGData.data.result[0].data[2000]);
+	// 		console.log(emissions,GHGData);
+	// 	}
+	// },[GHGData]);
+
+
+	// //loading map data into variables
+	// useEffect(() => {
+	// 	if(mapData.config){
+	// 		setMap(mapData.data);
+	// 	}
+	// },[mapData]);
 
 	return(
 		<main>
-			<Header type={type}/>
-			<img className="map" src={mapUralInitial} alt = "static map"/>
+			<img className="map" src={mapUrl} alt = "static map"/>
 			<div className = "data_circle"> 
-				<p>{emissions}</p>
 			</div>
 		</main>
-	)
+	);
 }
 
 
