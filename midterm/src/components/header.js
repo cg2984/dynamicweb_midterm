e03eccf0c29b29779ce73a0ba88129aaf97d887b
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2020-04-01 10:08:44 -0400

    adding a new header

--- midterm/src/components/header.js
+++ midterm/src/components/header.js
@@ -3,24 +3,40 @@ import React, {useEffect,useState} from 'react';
 function Header() {
 	return(
 		<header className = "header">
-			<div className = "button">
-				<a href = "/?type=industrial">INDUSTRIAL</a>
-			</div>
-			<div className = "button">
-				<a href = "/?type=commercial">COMMERCIAL</a>
-			</div>
-			<div className = "button">
-				<a href = "/?type=electric">ELECTRIC</a>
-			</div>
-			<div className = "button">
-				<a href = "/?type=residential">RESIDENTIAL</a>
-			</div>
-			<div className = "button">
-				<a href = "/?type=transportation">TRANSPORTATION</a>
-			</div>
-			<div className = "button">
-				<a href = "/?type=total">TOTAL</a>
-			</div>
+			<nav className = "types">
+				<div className = "button">
+					<a href = "/?type=industrial">INDUSTRIAL</a>
+				</div>
+				<div className = "button">
+					<a href = "/?type=commercial">COMMERCIAL</a>
+				</div>
+				<div className = "button">
+					<a href = "/?type=electric">ELECTRIC</a>
+				</div>
+				<div className = "button">
+					<a href = "/?type=residential">RESIDENTIAL</a>
+				</div>
+				<div className = "button">
+					<a href = "/?type=transportation">TRANSPORTATION</a>
+				</div>
+				<div className = "button">
+					<a href = "/?type=total">TOTAL</a>
+				</div>
+			</nav>
+			<nav className = "years">
+				<div className = "button">
+					<a href = "&year=1980">1980</a>
+				</div>
+				<div className = "button">
+					<a href = "&year=1990">1990</a>
+				</div>
+				<div className = "button">
+					<a href = "&year=2000">2000</a>
+				</div>
+				<div className = "button">
+					<a href = "&year=2017">2017</a>
+				</div>
+			</nav>
 		</header>
 	)
 }
